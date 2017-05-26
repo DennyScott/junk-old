82bed66695ff3fab87e6f397e22cfefe26657491
Denny Scott <dennyscott301@gmail.com>
2017-05-25 21:35:54 -0500

    remove react header

--- src/App.js
+++ src/App.js
@@ -7,13 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
-        </div>
-        <p className="App-intro">
-          Hello Electron!
-        </p>
         <div id="phaser"></div>
       </div>
     );
