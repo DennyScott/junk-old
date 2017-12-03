880e2083be383ec5fd4421487280d281a2ad18c7
Travis Scott <traviss@iqmetrix.com>
2017-12-03 00:10:56 -0600

    Fixed up code smells

--- src/components/explorer/explorer.js
+++ src/components/explorer/explorer.js
@@ -15,32 +15,34 @@ class Explorer extends Component {
     program: PropTypes.object.isRequired,
   };
 
+  clickDirectory(key) {
+    this.props.openFolder(
+      this.props.program.windowId,
+      key,
+      this.props.program.currentDirectory[key],
+    )
+  }
+
+  createDirectory(key) {
+    return (
+      <div
+        key={key}
+        onClick={() => this.clickDirectory(key)}
+      >
+        {key}
+      </div>
+    )
+  }
+
   render() {
     return (
       <ProgramWindow program={this.props.program} displayMenu>
         <NavigationBar
           upButtonClick={() => this.props.upFolder(this.props.program.windowId)}
-          backButtonClick={() =>
-            this.props.backFolder(this.props.program.windowId)
-          }
-          forwardButtonClick={() =>
-            this.props.forwardFolder(this.props.program.windowId)
-          }
+          backButtonClick={() => this.props.backFolder(this.props.program.windowId)}
+          forwardButtonClick={() => this.props.forwardFolder(this.props.program.windowId)}
         />
-        {Object.keys(this.props.program.currentDirectory).map(key => (
-          <div
-            key={key}
-            onClick={() =>
-              this.props.openFolder(
-                this.props.program.windowId,
-                key,
-                this.props.program.currentDirectory[key],
-              )
-            }
-          >
-            {key}
-          </div>
-        ))}
+        {Object.keys(this.props.program.currentDirectory).map(key => this.createDirectory(key))}
       </ProgramWindow>
     );
   }
